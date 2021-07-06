e491f4d2d05e5464f8def2a697d6f9ab69255e21
Brian Sai <brianwsai@gmail.com>
2021-07-06 12:57:12 -0700

    Initial styling of appbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import { Typography, AppBar } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
 import VideoPlayer from './components/VideoPlayer';
 import Options from './components/Options';
 import Notifications from './components/Notifications';
 
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: '10vh',
+    padding: '10px',
+
+    [theme.breakpoints.down('xs')]: {
+      width: '90%',
+    },
+  },
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+  },
+}));
+
 const App = () => {
+  const classes = useStyles();
+  const { appBar, wrapper } = classes;
   return (
-    <div>
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">
-          Video Chat
+    <div className={wrapper}>
+      <AppBar position="static" color="inherit" className={appBar}>
+        <Typography variant="h5" align="center">
+          WeVideo
         </Typography>
       </AppBar>
       <VideoPlayer />
